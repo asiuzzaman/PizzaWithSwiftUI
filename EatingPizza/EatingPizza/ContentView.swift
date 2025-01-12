7d92f2f00e4caed3800bee0a41841d33e10fe6b4
asiuzzaman <zaman1304006@gmail.com>
2025-01-12 17:27:32 +0600

    Manipulate UI using forLoop, Scrollview

--- EatingPizza/EatingPizza/ContentView.swift
+++ EatingPizza/EatingPizza/ContentView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        let orders: [Int] = [1,2,3,4,5, 7, 10]
         VStack {
-
             ZStack {
                 Image("surfBanner")
                     .resizable()
@@ -18,21 +18,45 @@ struct ContentView: View {
                 Text("Pizza company")
                     .background()
             }
-            Text("Order Pizza")
-                .font(.title)
-            HStack(alignment: .firstTextBaseline){
-                Text("Your order Item")
+            Image(systemName: orders.isEmpty ?"cart": "cart.fill")
+            HStack {
+                Text("Order Pizza")
+                    .font(.title)
                 Spacer()
-                Text("$500.00")
             }
-            HStack(
-                alignment: .top,
-                spacing: 15
-            ){
-                Image("0_lg").resizable().scaledToFit()
-                Text("Margarita")
+
+            ScrollView {
+                ForEach(orders, id:\.self) { order in
+                    HStack(alignment: .firstTextBaseline){
+                        Text("Your order Item: \(order)")
+                        Spacer()
+                        Text(19.90,format: .currency(code: "USD"))
+                    }
+                }
             }
-            Spacer()
+
+            VStack {
+                Image(systemName: "rectangle.fill").font(.largeTitle)
+                Text("Delivery")
+                Text("Free")
+            }
+
+            ScrollView {
+                ForEach(1...25, id:\.self) { item in
+                    HStack(
+                        alignment: .top,
+                        spacing: 15
+                    ){
+                        Image(systemName: "\(item).circle.fill").font(.largeTitle)
+                        VStack(alignment: .leading) {
+                            Text("Margarita")
+                            Text("Description")
+                        }
+                    }
+                }
+
+                Spacer()
+            }
         }
         .padding()
     }
