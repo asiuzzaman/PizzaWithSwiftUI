72f2447405428e741b138cc0ff3dccec6c0b174b
asiuzzaman <zaman1304006@gmail.com>
2025-01-12 15:30:25 +0600

    Creating project

---
+++ EatingPizza/EatingPizza/EatingPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  EatingPizzaApp.swift
+//  EatingPizza
+//
+//  Created by Md. Asiuzzaman on 12/1/25.
+//
+
+import SwiftUI
+
+@main
+struct EatingPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
